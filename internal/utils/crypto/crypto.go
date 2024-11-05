84b03a8b5e63df27a7d4c326940990d4a16c91c6
anbui <buiphuan021100@gmail.com>
2024-11-05 16:38:21 +0700

    feat 33-34: JWT authentication

--- internal/utils/crypto/crypto.go
+++ internal/utils/crypto/crypto.go
@@ -29,3 +29,10 @@ func HashPassword(password string, salt string) string {
 	hashPass := sha256.Sum256([]byte(saltedPassword))
 	return hex.EncodeToString(hashPass[:])
 }
+
+// "store password" is saved in the database, "password" is input data
+
+func MatchingPassword(storeHash, password string, salt string) bool {
+	hashPassword := HashPassword(password, salt)
+	return hashPassword == storeHash
+}
--- internal/utils/util.go
+++ internal/utils/util.go
@@ -1,7 +1,20 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/google/uuid"
+)
 
 func GetUserKey(hashString string) string {
 	return fmt.Sprintf("u:%s:otp", hashString)
+}
+
+func GenerateUUID(userId int) string {
+	newUUID := uuid.New()
+	// covert UUID to string, remove "-" character
+	uuidString := strings.ReplaceAll((newUUID).String(), "", "")
+	return strconv.Itoa(userId) + "clitoken" + uuidString
 }
